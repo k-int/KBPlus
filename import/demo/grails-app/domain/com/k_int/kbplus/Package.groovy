e60d960535c655e086affa3de6fa72cf387e0201
ianibo <ianibbo@googlemail.com>
2012-04-10 12:15:08 +0100

    Added domain classes

---
+++ import/demo/grails-app/domain/com/k_int/kbplus/Package.groovy
@@ -0,0 +1,7 @@
+package com.k_int.kbplus
+
+class Package {
+
+    static constraints = {
+    }
+}
---
+++ import/demo/grails-app/domain/com/k_int/kbplus/PlatformTIPP.groovy
@@ -0,0 +1,7 @@
+package com.k_int.kbplus
+
+class PlatformTIPP {
+
+    static constraints = {
+    }
+}
