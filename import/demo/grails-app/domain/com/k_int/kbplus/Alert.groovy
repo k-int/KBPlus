c47026b95b4cdd6293bc7debd11ec6b04100418c
Ian Ibbotson <ianibbo@gmail.com>
2012-04-17 11:55:20 +0100

    added alert domain classes

---
+++ import/demo/grails-app/domain/com/k_int/kbplus/Alert.groovy
@@ -0,0 +1,7 @@
+package com.k_int.kbplus
+
+class Alert {
+
+    static constraints = {
+    }
+}
---
+++ import/demo/test/unit/com/k_int/kbplus/AlertTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.kbplus
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Alert)
+class AlertTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
