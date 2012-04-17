8815024aa223ad68ae41f4debbfb08779bc7a444
Ian Ibbotson <ianibbo@gmail.com>
2012-04-17 11:54:36 +0100

    Added docs alert

--- import/demo/grails-app/domain/com/k_int/kbplus/Doc.groovy
+++ import/demo/grails-app/domain/com/k_int/kbplus/Doc.groovy
@@ -4,12 +4,14 @@ class Doc {
 
   RefdataValue status
   RefdataValue type
+  Alert alert
 
   static mapping = {
                 id column:'doc_id'
            version column:'doc_version'
             status column:'doc_status_rv_fk'
               type column:'doc_type_rv_fk'
+             alert column:'doc_alert_fk'
   }
 
   static constraints = {
