29f29ce023411cee66547fbc20fed185e47cd93e
ianibo <ianibbo@googlemail.com>
2012-04-23 14:19:56 +0100

    Started work on orgs data import

---
+++ import/processing/processing/OrgsData.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/groovy
+
+// @GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
+@Grapes([
+  @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
+  @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
+])
+
+import org.apache.log4j.*
+import au.com.bytecode.opencsv.CSVReader
+import java.text.SimpleDateFormat
+
+
+def starttime = System.currentTimeMillis();
+def possible_date_formats = [
+  new SimpleDateFormat('dd/MM/yy'),
+  new SimpleDateFormat('yyyy/MM'),
+  new SimpleDateFormat('yyyy')
+];
+
+
+// Setup mongo
+def options = new com.mongodb.MongoOptions()
+options.socketKeepAlive = true
+options.autoConnectRetry = true
+options.slaveOk = true
+def mongo = new com.gmongo.GMongo('127.0.0.1', options);
+def db = mongo.getDB('kbplus_ds_reconciliation')
+
+if ( db == null ) {
+  println("Failed to configure db.. abort");
+  system.exit(1);
+}
+
+
+// To clear down the gaz: curl -XDELETE 'http://localhost:9200/gaz'
+// CSVReader r = new CSVReader( new InputStreamReader(getClass().classLoader.getResourceAsStream("./IEEE_IEEEIEL_2012_2012.csv")))
+println("Processing ${args[0]}");
+CSVReader r = new CSVReader( new InputStreamReader(new FileInputStream(args[0])))
+
+def bad_rows = []
+
+String [] nl;
+
+String [] so_header_line = r.readNext()
+
+println("Read column headings: ${so_header_line}");
+
+def stats = [:]
+
+int rownum = 0;
+
+while ((nl = r.readNext()) != null) {
+  rownum++
+  boolean bad = false;
+  String badreason = null;
+  println("Processing ${nl}");
+}
+
