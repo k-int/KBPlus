3822e9a7aba0ad77ac65d492aec069bd6ab82410
ianibo <ianibbo@googlemail.com>
2012-05-21 10:31:37 +0100

    added license processing

---
+++ import/processing/processing/LicenseData.groovy
@@ -0,0 +1,79 @@
+#!/usr/bin/groovy
+
+// @GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
+@Grapes([
+  @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
+  @Grab(group='com.gmongo', module='gmongo', version='0.9.5')
+])
+
+import org.apache.log4j.*
+import au.com.bytecode.opencsv.CSVReader
+import java.text.SimpleDateFormat
+
+
+def starttime = System.currentTimeMillis();
+def possible_date_formats = [
+  new SimpleDateFormat('dd/MM/yy'),
+  new SimpleDateFormat('yyyy/MM'),
+  new SimpleDateFormat('yyyy')
+];
+
+
+// Setup mongo
+def options = new com.mongodb.MongoOptions()
+options.socketKeepAlive = true
+options.autoConnectRetry = true
+options.slaveOk = true
+def mongo = new com.gmongo.GMongo('127.0.0.1', options);
+def db = mongo.getDB('kbplus_ds_reconciliation')
+
+if ( db == null ) {
+  println("Failed to configure db.. abort");
+  System.exit(1);
+}
+
+
+// To clear down the gaz: curl -XDELETE 'http://localhost:9200/gaz'
+// CSVReader r = new CSVReader( new InputStreamReader(getClass().classLoader.getResourceAsStream("./IEEE_IEEEIEL_2012_2012.csv")))
+println("Processing ${args[0]}");
+CSVReader r = new CSVReader( new InputStreamReader(new FileInputStream(args[0])))
+
+def bad_rows = []
+
+String [] nl;
+
+String [] lic_header_line = r.readNext()
+
+println("Read column headings: ${so_header_line}");
+
+def stats = [:]
+
+int rownum = 0;
+int lic_bad = 0;
+int processed = 0;
+
+while ((nl = r.readNext()) != null) {
+  rownum++
+  boolean bad = false;
+  String badreason = null;
+  boolean has_data = false
+}
+
+println("Stats: ${stats}");
+
+def statsfile = new File("stats.txt");
+statsfile << "${new Date().toString()}\n\nLicense import\n--------\n\n"
+stats.each { stat ->
+  statsfile << "${stat.key} : ${stat.value}\n"
+}
+
+if ( bad_rows.size() > 0 ) {
+  println("file contained bad rows, dumping to ${args[0]}_BAD");
+  File badfile = new File("${args[0]}_BAD");
+  bad_rows.each { row ->
+    badfile << row.row
+    badfile << ",\"row ${row.rownum}/${rownum} - ${row.reason}\"\n"
+  }
+}
+
+println("All done processing for ${args[0]}");
