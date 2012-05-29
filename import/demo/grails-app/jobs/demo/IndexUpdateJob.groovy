43731e5f63114c8aa9d924d231943b432ac95517
Ian Ibbotson <ianibbo@gmail.com>
2012-05-29 18:54:30 +0100

    added index update task

---
+++ import/demo/grails-app/jobs/demo/IndexUpdateJob.groovy
@@ -0,0 +1,13 @@
+package demo
+
+
+
+class IndexUpdateJob {
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        // execute job
+    }
+}
