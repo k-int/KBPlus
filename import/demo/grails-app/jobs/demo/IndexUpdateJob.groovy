6f4f1c2e9cfcb04c42f56e5d8fbe462fe3fd0c30
Ian Ibbotson <ianibbo@googlemail.com>
2012-05-30 22:51:04 +0100

    Final work on ES updated

--- import/demo/grails-app/jobs/demo/IndexUpdateJob.groovy
+++ import/demo/grails-app/jobs/demo/IndexUpdateJob.groovy
@@ -5,6 +5,7 @@ package demo
 class IndexUpdateJob {
 
   def mongoService 
+  def ESWrapperService
 
   static triggers = {
     cron name:'cronTrigger', startDelay:20000, cronExpression: "0 0/5 * * * ?"
@@ -12,20 +13,32 @@ class IndexUpdateJob {
 
   def execute() {
 
-    def start_time = System.currentTimeMillis();
-    def mdb = mongoService.getMongo().getDB('kbplus_ds_reconciliation')
-
-    updateOrgs(mdb, com.k_int.kbplus.Org.class);
-    updateOrgs(mdb, com.k_int.kbplus.TitleInstance.class);
-
-    // execute task
     log.debug("Execute IndexUpdateJob starting at ${new Date()}");
-    def elapsed = System.currentTimeMillis() - start_time;
+    def start_time = System.currentTimeMillis();
 
+    def mdb = mongoService.getMongo().getDB('kbplus_ds_reconciliation')
+    org.elasticsearch.groovy.node.GNode esnode = ESWrapperService.getNode()
+    org.elasticsearch.groovy.client.GClient esclient = esnode.getClient()
+
+    updateOrgs(mdb, esclient, com.k_int.kbplus.Org.class) { org ->
+      def result = [:]
+      result._id = org.impId
+      result.name = org.name
+      result.sector = org.sector
+      result
+    }
+
+    updateOrgs(mdb, esclient, com.k_int.kbplus.TitleInstance.class) { ti ->
+      def result = [:]
+      result._id = ti.impId
+      result.title = ti.title
+    }
+
+    def elapsed = System.currentTimeMillis() - start_time;
     log.debug("IndexUpdateJob completed in ${elapsed}ms at ${new Date()}");
   }
 
-  def updateOrgs(mdb, domain) {
+  def updateOrgs(mdb, esclient, domain, recgen_closure) {
 
     def timestamp_record = mdb.timestamps.findOne(domain:domain.name)
     def max_ts_so_far = 0;
@@ -43,11 +56,23 @@ class IndexUpdateJob {
     Date from = new Date(timestamp_record.latest);
     def qry = domain.findAllByLastUpdatedGreaterThan(from);
     qry.each { i ->
-      log.debug(i);
+      // log.debug(i);
       max_ts_so_far = i.lastUpdated.getTime() ?: 0
+
+      def idx_record = recgen_closure(i)
+
+      // log.debug("Generated record ${idx_record}");
+      def future = esclient.index {
+        index "kbplus"
+        type "domain.name"
+        id idx_record['_id']
+        source idx_record
+      }
     }
 
     timestamp_record.latest = max_ts_so_far
-    // mdb.timestamps.save(timestamp_record);
+    mdb.timestamps.save(timestamp_record);
+
+    log.debug("Completed processing on ${domain.name}");
   }
 }
