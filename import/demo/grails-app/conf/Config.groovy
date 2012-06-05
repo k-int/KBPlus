acc0f187322aefde84b880a57997fe34a0e5d0ec
ianibo <ianibbo@googlemail.com>
2012-06-05 15:16:10 +0100

    updated

--- import/demo/grails-app/conf/Config.groovy
+++ import/demo/grails-app/conf/Config.groovy
@@ -10,6 +10,8 @@ grails.config.locations = [ // "classpath:${appName}-config.properties",
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+System.out.println("conf: ${grails.config.locations}");
+
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
