5e18fcf47db69b8436ea4f0f6b150981f96433b8
ianibo <ianibbo@googlemail.com>
2012-06-08 09:39:57 +0100

    Config for pre-auth user dets

--- import/demo/grails-app/conf/spring/resources.groovy
+++ import/demo/grails-app/conf/spring/resources.groovy
@@ -1,5 +1,9 @@
 // Place your Spring DSL code here
 beans = {
+  preAuthUserDets(org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesUserDetailsService){
+  }
+
   preAuthenticatedAuthenticationProvider(org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider) {
+    preAuthenticatedUserDetailsService = ref('preAuthUserDets');
   }
 }
