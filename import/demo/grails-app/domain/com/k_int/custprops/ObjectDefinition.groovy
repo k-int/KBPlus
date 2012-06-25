31f890a368fe2a16822f1c544877074cffb66110
ianibo <ianibbo@googlemail.com>
2012-06-25 12:28:29 +0100

    updated

--- import/demo/grails-app/domain/com/k_int/custprops/ObjectDefinition.groovy
+++ import/demo/grails-app/domain/com/k_int/custprops/ObjectDefinition.groovy
@@ -2,6 +2,12 @@ package com.k_int.custprops
 
 class ObjectDefinition extends PropertyDefinition {
 
-    static constraints = {
-    }
+  static hasMany = [properties:PropertyDefinition]
+
+  static constraints = {
+  }
+
+  static mapping = {
+    properties joinTable: [name:'obj_prop_members', key:'obj_prop', column:'member_prop']
+  }
 }
