f70589badf36f673b9d0988aaaf874bf42102a15
ianibo <ianibbo@googlemail.com>
2012-07-19 14:27:40 +0100

    More work on alerts

--- import/demo/grails-app/services/com/k_int/kbplus/AlertsService.groovy
+++ import/demo/grails-app/services/com/k_int/kbplus/AlertsService.groovy
@@ -15,6 +15,17 @@ class AlertsService {
     // List all institutions this user is attached to
     user.affiliations.each { ua ->
       def org = ua.org;
+
+      // For each institution, iterate through all licenses attached to that institution
+      def licensee_role = RefdataCategory.lookupOrCreate('Organisational Role','Licensee');
+      OrgRole.findAllByOrgAndRoleType(result.institution, licensee_role).each { or ->
+        if ( or.lic?.status?.value!='Deleted' ) {
+          // license under question is in or.lic - See if there are any notes attached to that license that this user can see
+        }
+      }
+
+
+
     }
   }
 }
