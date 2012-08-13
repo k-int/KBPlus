8f8a71fce99f35307e92799045826f45047606d4
Ian Ibbotson <ianibbo@googlemail.com>
2012-08-13 20:57:09 +0100

    Added license status entries to refdata. closes #173

--- import/schema/refdata.sql
+++ import/schema/refdata.sql
@@ -51,6 +51,10 @@ insert into refdata_category(rdc_id, rdc_description) VALUES (11,'License Status
 insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Current');
 insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Unknown');
 insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Deleted');
+insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Expired');
+insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Under Negotiation');
+insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Under Consideration');
+insert into refdata_value(rdv_owner, rdv_value) VALUES ( 11, 'Awaiting Signature');
 
 insert into refdata_category(rdc_id, rdc_description) VALUES (12,'Subscription Type');
 insert into refdata_value(rdv_owner, rdv_value) VALUES ( 12, 'Subscription Taken');
