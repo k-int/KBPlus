1e2e6b1221969a7e627d32661a8583dec65db514
ianibo <ianibbo@googlemail.com>
2012-09-24 16:21:51 +0100

    Removed tabs from url mappings

--- import/demo/grails-app/conf/UrlMappings.groovy
+++ import/demo/grails-app/conf/UrlMappings.groovy
@@ -1,25 +1,25 @@
 class UrlMappings {
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+  static mappings = {
+    "/$controller/$action?/$id?"{
+      constraints {
+        // apply constraints here
+      }
+    }
 
-                "/myInstitutions/$shortcode/$action"(controller:'myInstitutions')
-                "/myInstitutions/$shortcode/$action/$id"(controller:'myInstitutions')
+    "/myInstitutions/$shortcode/$action"(controller:'myInstitutions')
+    "/myInstitutions/$shortcode/$action/$id"(controller:'myInstitutions')
 
-		// "/"(controller:"home")
-		"/"(view:"/publichome")
-		"500"(view:'/error')
+    // "/"(controller:"home")
+    "/"(view:"/publichome")
+    "500"(view:'/error')
  
-		"/about"(view:"/about")
-        "/terms-and-conditions"(view:"/terms-and-conditions")
-        "/privacy-policy"(view:"/privacy-policy")
-        "/freedom-of-information-policy"(view:"/freedom-of-information-policy")
-        "/contact-us"(view:"/contact-us")
-		"/publichome"(view:"/publichome")
-		"/signup"(view:"/signup")
-	}
+    "/about"(view:"/about")
+    "/terms-and-conditions"(view:"/terms-and-conditions")
+    "/privacy-policy"(view:"/privacy-policy")
+    "/freedom-of-information-policy"(view:"/freedom-of-information-policy")
+    "/contact-us"(view:"/contact-us")
+    "/publichome"(view:"/publichome")
+    "/signup"(view:"/signup")
+  }
 }
