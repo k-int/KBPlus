74a2d8dd2e65af9c9af5f0b43958c7b20bbc9f82
ianibo <ianibbo@googlemail.com>
2012-11-13 10:10:38 +0000

    Fixed csv export code on subscriptions page

--- app/grails-app/controllers/com/k_int/kbplus/SubscriptionDetailsController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/SubscriptionDetailsController.groovy
@@ -61,44 +61,78 @@ class SubscriptionDetailsController {
 
     result.entitlements = IssueEntitlement.executeQuery("select ie "+base_qry, qry_params, [max:result.max, offset:result.offset]);
 
+    def formatter = new java.text.SimpleDateFormat("yyyy/MM/dd")
     log.debug("subscriptionInstance returning... ${result.num_sub_rows} rows ");
     withFormat {
-      // json {
-      //         render people as JSON
-      //      }
-      // }
-      // xml {
-      //      render people as XML
-      // }
       html result
       csv {
-         def formatter = new java.text.SimpleDateFormat("yyyy/MM/dd")
          def jc_id = result.subscriptionInstance.getSubscriber()?.getIdentifierByType('JC')?.value
 
-         response.setHeader("Content-disposition", "attachment; filename=${result.subscriptionInstance.id}.csv")
+         response.setHeader("Content-disposition", "attachment; filename=${result.subscriptionInstance.identifier}.csv")
          response.contentType = "text/csv"
          def out = response.outputStream
          out.withWriter { writer ->
-           // Output the header information
+           def tsdate = result.subscriptionInstance.startDate ? formatter.format(result.subscriptionInstance.startDate) : ''
+           def tedate = result.subscriptionInstance.endDate ? formatter.format(result.subscriptionInstance.endDate) : ''
            if ( ( params.omitHeader == null ) || ( params.omitHeader != 'Y' ) ) {
-             writer.write("FileType,SpecVersion,JD_ID,TermStartDate,TermEndDate,SubURI\n")
-             writer.write("\"${result.subscriptionInstance.type.value}\",\"2.0\",${jc_id},\"${result.subscriptionInstance.startDate}\",\"${result.subscriptionInstance.endDate}\",\"uri://kbplus/sub/${result.subscriptionInstance.id}\"\n")
+             writer.write("FileType,SpecVersion,JC_ID,TermStartDate,TermEndDate,SubURI,SystemIdentifier\n")
+             writer.write("${result.subscriptionInstance.type.value},\"2.0\",${jc_id?:''},${tsdate},${tedate},\"uri://kbplus/sub/${result.subscriptionInstance.identifier}\",${result.subscriptionInstance.impId}\n")
            }
 
            // Output the body text
-           writer.write("included_st,publication_title,print_identifier,online_identifier,date_first_issue_subscribed,num_first_vol_subscribed,num_first_issue_subscribed,date_last_issue_subscribed,num_last_vol_subscribed,num_last_issue_subscribed,embargo_info,core_title\n");
+           // writer.write("publication_title,print_identifier,online_identifier,date_first_issue_subscribed,num_first_vol_subscribed,num_first_issue_subscribed,date_last_issue_subscribed,num_last_vol_subscribed,num_last_issue_subscribed,embargo_info,title_url,first_author,title_id,coverage_note,coverage_depth,publisher_name\n");
+           writer.write("publication_title,print_identifier,online_identifier,date_first_issue_online,num_first_vol_online,num_first_issue_online,date_last_issue_online,num_last_vol_online,num_last_issue_online,title_url,first_author,title_id,embargo_info,coverage_depth,coverage_notes,publisher_name\n");
 
            result.entitlements.each { e ->
 
              def start_date = e.startDate ? formatter.format(e.startDate) : '';
              def end_date = e.endDate ? formatter.format(e.endDate) : '';
+             def title_doi = (e.tipp?.title?.getIdentifierValue('DOI'))?:''
+             def publisher = e.tipp?.title?.publisher
 
-             writer.write("Y,\"${e.tipp.title.title}\",\"${e.tipp?.title?.getIdentifierValue('ISSN')}\",\"${e.tipp?.title?.getIdentifierValue('eISSN')}\",${start_date},${e.startVolume?:''},${e.startIssue?:''},${end_date},${e.endVolume?:''},${e.endIssue?:''},${e.embargo?:''},${e.coreTitle}\n");
+             writer.write("\"${e.tipp.title.title}\",\"${e.tipp?.title?.getIdentifierValue('ISSN')?:''}\",\"${e.tipp?.title?.getIdentifierValue('eISSN')?:''}\",${start_date},${e.startVolume?:''},${e.startIssue?:''},${end_date},${e.endVolume?:''},${e.endIssue?:''},\"${e.tipp?.hostPlatformURL?:''}\",,\"${title_doi}\",\"${e.embargo?:''}\",\"${e.tipp?.coverageDepth?:''}\",\"${e.tipp?.coverageNote?:''}\",\"${publisher?.name?:''}\"\n");
            }
            writer.flush()
            writer.close()
          }
          out.close()
+      }
+      json {
+         def jc_id = result.subscriptionInstance.getSubscriber()?.getIdentifierByType('JC')?.value
+         def response = [:]
+         response.header = [:]
+         response.entitlements = []
+
+         response.header.type = result.subscriptionInstance.type.value
+         response.header.version = "2.0"
+         response.header.jcid = jc_id
+         response.header.url = "uri://kbplus/sub/${result.subscriptionInstance.identifier}"
+
+         result.entitlements.each { e ->
+           def start_date = e.startDate ? formatter.format(e.startDate) : '';
+           def end_date = e.endDate ? formatter.format(e.endDate) : '';
+           def title_doi = (e.tipp?.title?.getIdentifierValue('DOI'))?:''
+           def publisher = e.tipp?.title?.publisher
+
+           def entitlement = [:]
+           entitlement.title=e.tipp.title.title
+           entitlement.issn=e.tipp?.title?.getIdentifierValue('ISSN')
+           entitlement.eissn=e.tipp?.title?.getIdentifierValue('eISSN')
+           entitlement.startDate=start_date;
+           entitlement.endDate=end_date;
+           entitlement.startVolume=e.startVolume?:''
+           entitlement.endVolume=e.endVolume?:''
+           entitlement.startIssue=e.startIssue?:''
+           entitlement.endIssue=e.endIssue?:''
+           entitlement.embargo=e.embargo?:''
+           entitlement.titleUrl=e.tipp.hostPlatformURL?:''
+           entitlement.doi=title_doi
+           entitlement.coverageDepth = e.tipp.coverageDepth
+           entitlement.coverageNote = e.tipp.coverageNote
+           entitlement.publisher = publisher.name
+           response.entitlements.add(entitlement);
+         }
+         render response as JSON
       }
     }
   }
@@ -339,5 +373,5 @@ class SubscriptionDetailsController {
 
     redirect controller: 'subscriptionDetails', action:'documents', id:params.subId
   }
-
 }
+
