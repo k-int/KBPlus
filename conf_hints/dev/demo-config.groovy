fc08a9c7299e94fed6b664ad0137831ec4f26ad7
ianibo <ianibbo@googlemail.com>
2012-11-30 09:59:06 +0000

    Added example grails config file to conf_hints section with settings for a dev environment

---
+++ conf_hints/dev/demo-config.groovy
@@ -0,0 +1,7 @@
+localauth=true
+// dataSource.url="jdbc:mysql://localhost/KBPlus?autoReconnect=true&amp;characterEncoding=utf8"
+dataSource.user="..."
+dataSource.password='...'
+aggr.es.cluster='...'
+publicationService.baseurl='http://knowplus.edina.ac.uk:2012/kbplus/api'
+docstore='http://knowplus.edina.ac.uk:8080/oledocstore/KBPlusServlet'
