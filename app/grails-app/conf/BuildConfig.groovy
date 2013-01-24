c023dd690068d4f4503543345444e3c5f6313ad0
ianibo <ianibbo@googlemail.com>
2013-01-24 14:57:09 +0000

    upgraded resources plugin

--- app/grails-app/conf/BuildConfig.groovy
+++ app/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.0"
-        runtime ":resources:1.1.6"
+        runtime ":resources:1.2.RC2"
         // runtime ':twitter-bootstrap:2.0.2.25'
         runtime ':twitter-bootstrap:2.2.1'
         runtime ':fields:1.2'
