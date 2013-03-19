ab242698c741ba009b3271d3e41cbe06530615ca
mantas <mantas@student>
2013-03-19 14:11:35 +0000

    added (true) test

--- app/test/unit/com/k_int/kbplus/UploadControllerTests.groovy
+++ app/test/unit/com/k_int/kbplus/UploadControllerTests.groovy
@@ -18,42 +18,45 @@ class UploadControllerTests {
 
     def controller
     def test_list = [
-      [ filename:'resources/upload_so_test_001.csv', 
-        shouldProcess: false, 
-        message:'Testing missing SO Name'],
-      [ filename:'resources/upload_so_test_002.csv', 
-        shouldProcess: false, 
-        message:'Testing SO Name composed only of spaces']
+        [ filename:'resources/upload_so_test_001.csv', 
+            shouldProcess: false, 
+            message:'Testing missing SO Name'],
+        [ filename:'resources/upload_so_test_002.csv', 
+            shouldProcess: false, 
+            message:'Testing SO Name composed only of spaces'],
+        [ filename:'resources/upload_so_test_000.csv', 
+            shouldProcess: true, 
+            message:'This should pass']
     ];
 
     void setUp() {
-      controller = new UploadController()
+        controller = new UploadController()
     }
     
     // Info on mocking up file upload http://roshandawrani.wordpress.com/2011/02/03/grails-mock-testing-a-file-upload/
     void testMissingSubOfferedName() {
-     mockDomain(Package, [
-            [identifier: "001"],
-            [identifier: "002"],
-            [identifier: "003"] ])
+        mockDomain(Package, [
+                [identifier: "001"],
+                [identifier: "002"],
+                [identifier: "003"] ])
 
-     mockDomain(Subscription, [
-            [identifier: "001"],
-            [identifier: "002"],
-            [identifier: "003"] ])
+        mockDomain(Subscription, [
+                [identifier: "001"],
+                [identifier: "002"],
+                [identifier: "003"] ])
 
             
-      // fail "Implement me"
-      test_list.each { so_test_case ->
-        //log.debug("${so_test_case.message} Expecting shouldProcess to return ${so_test_case.shouldProcess}")
-        Resource resource = new ClassPathResource(so_test_case.filename)
-        def file = resource.getFile()
-        assert file.exists()
-        def input_stream = new FileInputStream(file)
-        def result = controller.readSubscriptionOfferedCSV(input_stream, 'filename.csv')
-        // Need to mock domain objects for tests
-        controller.validate(result);
-        assert result.processFile == so_test_case.shouldProcess
-      }
+        // fail "Implement me"
+        test_list.each { so_test_case ->
+            //log.debug("${so_test_case.message} Expecting shouldProcess to return ${so_test_case.shouldProcess}")
+            Resource resource = new ClassPathResource(so_test_case.filename)
+            def file = resource.getFile()
+            assert file.exists()
+            def input_stream = new FileInputStream(file)
+            def result = controller.readSubscriptionOfferedCSV(input_stream, 'filename.csv')
+            // Need to mock domain objects for tests
+            controller.validate(result);
+            assert result.processFile == so_test_case.shouldProcess
+        }
     }
 }
