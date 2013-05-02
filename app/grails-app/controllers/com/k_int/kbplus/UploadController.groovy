4b52cfdbf52ca9fecfde6231c50fbd95c7a31bd6
ianibo <ianibbo@googlemail.com>
2013-05-02 14:13:07 +0100

    Added warning for non-utf-8 character sets, added combo to allow user to select tab rather than csv input

--- app/grails-app/controllers/com/k_int/kbplus/UploadController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/UploadController.groovy
@@ -13,6 +13,7 @@ import org.apache.log4j.*
 import au.com.bytecode.opencsv.CSVReader
 import java.text.SimpleDateFormat
 
+import org.mozilla.universalchardet.UniversalDetector;
 
 
 class UploadController {
@@ -52,13 +53,13 @@ class UploadController {
     result.user = User.get(springSecurityService.principal.id)
     
     if ( request.method == 'POST' ) {
-      def upload_mime_type = request.getFile("soFile")?.contentType
-      def upload_filename = request.getFile("soFile")?.getOriginalFilename()
-      log.debug("Uploaded so type: ${upload_mime_type} filename was ${upload_filename}");
-      result.validationResult = readSubscriptionOfferedCSV(request.getFile("soFile")?.inputStream, upload_filename )
+
+
+      result.validationResult = readSubscriptionOfferedCSV(request)
+
       validate(result.validationResult)
       if ( result.validationResult.processFile == true ) {
-        log.debug("Passed first phase validation, continue...");
+        // log.debug("Passed first phase validation, continue...");
         processUploadSO(result.validationResult)
       }
     }
@@ -144,7 +145,8 @@ class UploadController {
         }
       }
           
-      log.debug("Lookup or create title ${tipp.id}");
+      // log.debug("Lookup or create title ${tipp.id}");
+
       tipp.title_obj = lookupOrCreateTitleInstance(tipp.id,tipp.publication_title,publisher);
       
       if ( tipp.title_obj && tipp.host_platform && new_pkg ) {
@@ -177,7 +179,7 @@ class UploadController {
             }
           }
           else {
-            log.debug("new TIPP Save OK ${dbtipp.id}");
+            // log.debug("new TIPP Save OK ${dbtipp.id}");
             tipp.messages.add([type:'alert-success',message:"New tipp created: ${dbtipp.id}"]);
             tipp.additional_platforms.each { ap ->
               PlatformTIPP pt = new PlatformTIPP(tipp:dbtipp,platform:ap.plat,titleUrl:ap.url,rel:ap.role)
@@ -195,7 +197,7 @@ class UploadController {
     }
 
     // Create an SO by creating a header and copying the tipps from this package into IE's
-    log.debug("Copying Package TIPPs into issue entitlements");
+    // log.debug("Copying Package TIPPs into issue entitlements");
     def new_pkg_id = new_pkg.id;
     new_pkg.discard();
     def session = sessionFactory.currentSession
@@ -221,7 +223,7 @@ class UploadController {
   }
     
   def lookupOrCreateTitleInstance(identifiers,title,publisher) {
-    log.debug("lookupOrCreateTitleInstance ${identifiers}, ${title}, ${publisher}");
+    // log.debug("lookupOrCreateTitleInstance ${identifiers}, ${title}, ${publisher}");
     def result = TitleInstance.lookupOrCreateViaIdMap(identifiers, title);
     if ( !result.getPublisher() ) {
       def pub_role = RefdataCategory.lookupOrCreate('Organisational Role', 'Publisher');
@@ -231,7 +233,7 @@ class UploadController {
 
     // ToDo: Check to see that all the identifiers we have
 
-    log.debug("Done: ${result}");
+    // log.debug("Done: ${result}");
     result;
   }
   
@@ -272,17 +274,35 @@ class UploadController {
     return false
   }
 
-  def readSubscriptionOfferedCSV(input_stream, upload_filename) {
+  def readSubscriptionOfferedCSV(request) {
 
     def result = [:]
     result.processFile=true
-    
+
+    def upload_mime_type = request.getFile("soFile")?.contentType
+    def upload_filename = request.getFile("soFile")?.getOriginalFilename()
+    log.debug("Uploaded so type: ${upload_mime_type} filename was ${upload_filename}");
+
+    def charset = checkCharset(request.getFile("soFile")?.inputStream)
+
+    def input_stream = request.getFile("soFile")?.inputStream
+
     // File level messages
     result.messages=[]
 
     log.debug("Reading Stream");
 
-    CSVReader r = new CSVReader( new InputStreamReader(input_stream, java.nio.charset.Charset.forName('UTF-8') ) )
+    if  ( ( charset != null ) && ( ! charset.equals('UTF-8') ) ) {
+      result.messages.add("** WARNING: Detected input character stream encoding: ${charset}. Expected UTF-8");
+    }
+
+    CSVReader r = null
+    if ( request.docstyle?.equals("tsv") ) {
+      r = new CSVReader( new InputStreamReader(input_stream, java.nio.charset.Charset.forName('UTF-8') ), '\t' )
+    }
+    else {
+      r = new CSVReader( new InputStreamReader(input_stream, java.nio.charset.Charset.forName('UTF-8') ) )
+    }
 
     String [] nl;
 
@@ -422,7 +442,7 @@ class UploadController {
     int counter = 0;
     upload.tipps.each { tipp ->
     
-      log.debug("Validate tipp: ${tipp}");
+      // log.debug("Validate tipp: ${tipp}");
 
       if ( ( tipp.publication_title == null ) || 
            ( tipp.publication_title.trim() == '' ) ) {
@@ -435,7 +455,7 @@ class UploadController {
       //  upload.processFile=false;
       //}
       
-      log.debug("tipp id = ${tipp.id}");
+      // log.debug("tipp id = ${tipp.id}");
             
       if (!tipp.id) {
         tipp.messages.add("Title (row ${counter}) does not contain a valid ID (at least one of \"id.issn\", \"id.isbn\", \"id.eissn\" required, found: ${tipp.id})");
@@ -639,7 +659,7 @@ class UploadController {
     def matched_title_ids = []
     idlist.each { id ->
       def title = TitleInstance.findByIdentifier([[namespace:id.key,value:id.value]])
-      log.debug("found title ${title}");
+      // log.debug("found title ${title}");
       if ( title != null ) {
         if ( matched_title_ids.contains(title.id) )  {
           //log.debug("Matched titles already contains that title");
@@ -654,18 +674,18 @@ class UploadController {
       }
     }
 
-    log.debug("Identifiers matched the following title ids: ${matched_title_ids}");
+    // log.debug("Identifiers matched the following title ids: ${matched_title_ids}");
     return matched_title_ids;
   }
   
   def checkTitleFingerprintMatch(matched_title_id, title_from_import_file, tipp, upload) {
-    log.debug("checkTitleFingerprintMatch ${matched_title_id}, ${title_from_import_file}");
+    // log.debug("checkTitleFingerprintMatch ${matched_title_id}, ${title_from_import_file}");
     def title_instance = TitleInstance.get(matched_title_id)
     def generated_key_title = TitleInstance.generateKeyTitle(title_from_import_file)
-    log.debug("checkTitleFingerprintMatch ${matched_title_id}, ${title_from_import_file} == ${generated_key_title}");
+    // log.debug("checkTitleFingerprintMatch ${matched_title_id}, ${title_from_import_file} == ${generated_key_title}");
 
-    if ( title_instance.keyTitle != generated_key_title ) {
-      tipp.messages.add("Matched title identifier does not pass title fingerprint match. titleid=${matched_title_id}, ${title_instance.keyTitle} != ${generated_key_title}");
+    if ( ! title_instance.keyTitle.equals(generated_key_title) ) {
+      tipp.messages.add("Matched title identifier does not pass title fingerprint match. titleid=${matched_title_id}, \"${title_instance.keyTitle}\" != \"${generated_key_title}\"");
       upload.processFile=false;
     }
   }
@@ -681,5 +701,39 @@ class UploadController {
     else {
       result.stats[counter]++
     }
+  }
+
+  def checkCharset(file_input_stream) {
+
+    def result = null;
+
+    byte[] buf = new byte[4096];
+
+    // (1)
+    UniversalDetector detector = new UniversalDetector(null);
+
+    // (2)
+    int nread;
+    while ((nread = file_input_stream.read(buf)) > 0 && !detector.isDone()) {
+      detector.handleData(buf, 0, nread);
+    }
+    // (3)
+    detector.dataEnd();
+
+    // (4)
+    String encoding = detector.getDetectedCharset();
+    if (encoding != null) {
+      result = encoding; 
+      System.out.println("Detected encoding = " + encoding);
+      if ( encoding.equals('WINDOWS-1252') ) {
+      }
+    } else {
+      System.out.println("No encoding detected.");
+    }
+
+    // (5)
+    detector.reset();
+
+    result
   }
 }
