b108177d8480abdc633078323ad6b9c85c9c3072
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-05-09 17:24:44 +0100

    Secured docstore URI. Closes #463

--- app/grails-app/controllers/com/k_int/kbplus/DocstoreController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/DocstoreController.groovy
@@ -4,6 +4,7 @@ class DocstoreController {
 
   def docstoreService
 
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
     def doc = Doc.findByUuid(params.id);
     if ( doc ) {
