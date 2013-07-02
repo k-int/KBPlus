6fb480a8e31985cb6a34edd2cb26803ab1297b99
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-07-02 14:02:44 +0100

    updated

--- app/grails-app/services/com/k_int/kbplus/TitleStatsService.groovy
+++ app/grails-app/services/com/k_int/kbplus/TitleStatsService.groovy
@@ -2,7 +2,13 @@ package com.k_int.kbplus
 
 class TitleStatsService {
 
-    def serviceMethod() {
-
+    def getTitleStats(title_id, org_id) {
+      def result = []
+      reasult;
     }
+
+  def fetchFromJUSP() {
+    // JUSP URLs of the form - 
+    // https://www.jusp.mimas.ac.uk/api/v1/Journals/Statistics/?jid=13471&sid=2&loginid=cra&startrange=2012-01&endrange=2012-12&granularity=summary   
+  }
 }
