1c4b377f6d792b558f1b972424cfdeb643217a94
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-07-10 17:29:56 +0100

    Work on query for jusp stats download

--- app/grails-app/services/com/k_int/kbplus/JuspSyncService.groovy
+++ app/grails-app/services/com/k_int/kbplus/JuspSyncService.groovy
@@ -13,21 +13,28 @@ class JuspSyncService {
 //   def internalDoSync() {
 
     // Select distinct list of Title+Provider (TIPP->Package-CP->ID[jusplogin] with jusp identifiers
-    def q = "select distinct tipp.title, po.org, tipp.pkg from TitleInstancePackagePlatform as tipp " +
-              "join tipp.pkg.orgs as po where po.roleType.value='Content Provider' " +
-              "and exists ( select tid from tipp.title.ids as tid where tid.identifier.ns.ns = 'jusp' ) " +
-              "and exists ( select oid from po.org.ids as oid where oid.identifier.ns.ns = 'juspsid' ) "
-                // And exists title.identifier where namespace = jusp
-                // And exists org.identifier where namespace = jusplogin
+    // def q = "select distinct tipp.title, po.org, tipp.pkg from TitleInstancePackagePlatform as tipp " +
+    //           "join tipp.pkg.orgs as po where po.roleType.value='Content Provider' " +
+    //           "and exists ( select tid from tipp.title.ids as tid where tid.identifier.ns.ns = 'jusp' ) " +
+    //           "and exists ( select oid from po.org.ids as oid where oid.identifier.ns.ns = 'juspsid' ) "
+    def q = "select distinct ie.tipp.title, po.org, orgrel.org from IssueEntitlement as ie " +
+              "join ie.tipp.pkg.orgs as po " +
+              "join ie.subscription.orgRelations as orgrel "+
+              "join ie.tipp.title.ids as jusptid where jusptid.identifier.ns.ns = 'jusp' "+
+              "and po.roleType.value='Content Provider' "+
+              "and exists ( select oid from po.org.ids as oid where oid.identifier.ns.ns = 'juspsid' ) " +
+              "and orgrel.roleType.value = 'Subscriber' " +
+              "and exists ( select sid from orgrel.org.ids as sid where sid.identifier.ns.ns = 'jusplogin' ) "
 
     log.debug("JUSP Sync Task - Running query ${q}");
 
-    def l1 = TitleInstancePackagePlatform.executeQuery(q)
+    def l1 = IssueEntitlement.executeQuery(q)
 
     l1.each { to ->
-      log.debug("Processing titile/provider pair: ${to[0].title}, ${to[1].name} ${to[2].id}");
+      log.debug("Processing titile/provider pair: ${to[0].title}, ${to[1].name}");
       log.debug(" -> Title jusp id: ${to[0].getIdentifierValue('jusp')}");
-      log.debug(" -> Suppllirt jusp did: ${to[1].getIdentifierByType('juspsid')}");
+      log.debug(" -> Suppllier jusp id: ${to[1].getIdentifierByType('juspsid').value}");
+      log.debug(" -> Subscriber jusp id: ${to[2].getIdentifierByType('jusplogin').value}");
     }
 
  
