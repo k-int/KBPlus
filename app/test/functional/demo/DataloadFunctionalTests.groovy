6cf4c84d8313188a9dfa3696b5a05028cd3ccbb6
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-09-17 09:42:35 +0100

    Some progress on functional test

--- app/test/functional/demo/DataloadFunctionalTests.groovy
+++ app/test/functional/demo/DataloadFunctionalTests.groovy
@@ -1,13 +1,12 @@
 package demo
 
 import com.grailsrocks.functionaltest.*
-import com.gargoylesoftware.htmlunit.BrowserVersion
 
 
 class DataloadFunctionalTests extends BrowserTestCase {
 
     void testSomeWebsiteFeature() {
-        // setBrowser(BrowserVersion.CHROME)
+        setBrowser("CHROME")
         // setBrowser(BrowserVersion.CHROME_16)
         // Here call get(uri) or post(uri) to start the session
         // and then use the custom assertXXXX calls etc to check the response
@@ -17,18 +16,20 @@ class DataloadFunctionalTests extends BrowserTestCase {
         // assertContentContains 'the expected text'
         get('/login')
 
-        // assertNotNull page.forms['loginForm']
+        assertNotNull page.forms['loginForm']
 
-        form('loginForm') {
+        form('#loginForm') {
           j_username = "admin"
           j_password = "admin"
           click "submit"
         }
 
+        log.debug("Create user");
+
         get('/userDetails/create')
 
         // Create TestUserA - We will assign perms later on
-        form('createUserForm') {
+        form('#createUserForm') {
           username='TestUserA';
           display='TestUserA';
           password='password'
