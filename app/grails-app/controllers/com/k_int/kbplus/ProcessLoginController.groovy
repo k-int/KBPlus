2a18dde63a6850d32dd9cb68472d7f77ceac4ac1
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-09-28 11:30:33 +0100

    Added some error trapping to try and understand this auth edge case

--- app/grails-app/controllers/com/k_int/kbplus/ProcessLoginController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/ProcessLoginController.groovy
@@ -113,7 +113,13 @@ class ProcessLoginController {
       log.debug(ediAuthTokenMap)
 
       if ( ( params.ea_context ) && ( params.ea_context.trim().length() > 0 ) ) {
-        response_str="${params.ea_context}?ediauthToken=${tok}"
+        if ( params.ea_context.firstIndexOf('?') > 0 ) {
+          log.error("** EDGE CASE - ea_context already contains a query string: ${params.ea_context} **");
+          response_str="${params.ea_context}&ediauthToken=${tok}"
+        }
+        else {
+          response_str="${params.ea_context}?ediauthToken=${tok}"
+        }
       }
       else {
         response_str="http://knowplus.edina.ac.uk/kbplus/?ediauthToken=${tok}"
