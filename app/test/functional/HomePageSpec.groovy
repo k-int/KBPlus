cb39840293ecb4ce669dfba2f5fe28e8f014ccca
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-12-14 23:35:19 +0000

    Add home page spec

---
+++ app/test/functional/HomePageSpec.groovy
@@ -0,0 +1,81 @@
+import geb.spock.GebReportingSpec
+import spock.lang.*
+import pages.*
+
+@Stepwise
+class HomePageSpec extends GebReportingSpec {
+        
+  def "The KBPlus Home Page Displays OK"() {
+    when:
+    go "/demo"
+    then:
+    browser.page.title=="Knowledge Base+"
+  }
+
+  def "Check Administrative Login Prompt Is Working"() {
+    when:
+    // go "/demo/home/index"
+    go "/demo/profile/index"
+    report "login screen"
+    $("form").j_username='admin'
+    $("form").j_password='admin'
+    $("#submit").click()
+
+    then:
+    browser.page.title.startsWith "KB+ User Profile"
+  }
+
+  def "Check Add organisation works as expected"() {
+    when:
+    go "/demo/org/create"
+    $("form").name='Functional Test Organisation'
+    $("form").impId='FunctionalTestOrganisation'
+    $("form").sector='Higher Education'
+    $("#SubmitButton").click()
+    then:
+    browser.page.title.startsWith "Show Org"
+  }
+
+  def "Check Request Affiliation"() {
+    when:
+    go '/demo/profile/index'
+    $('form',1).org='Functional Test Organisation'
+    $('form',1).formalRole='Editor'
+    $('#submitARForm').click()
+    then:
+    browser.page.title.startsWith "KB+ User Profile"
+  }
+
+  def "Check Affiliation Approval"() {
+    when:
+    go '/demo/admin/manageAffiliationRequests'
+    $('td', text:'Functional Test Organisation').parent().find('a', text:'Approve').click()
+    then:
+    browser.page.title.startsWith "KB+ Manage Affiliation Requests"
+  }
+
+  def "Check user sets default institution"() {
+    when:
+    go '/demo/profile/index'
+    $('form').defaultDash='Functional Test Organisation'
+    then:
+    browser.page.title.startsWith "KB+ User Profile"
+  }
+
+  def "Check that home sends user to default inst"() {
+    when:
+    go '/demo/home/index'
+    then:
+    browser.page.title.startsWith "KB+ Institutional Dash :: Functional Test Organisation"
+  }
+
+  def "Test Package Import"() {
+    when:
+    go '/demo/upload/reviewSO'
+    $('form').soFile='/tmp/Art Journals_Master_2014.csv'
+    $('button').click()
+    then:
+    browser.page.title.startsWith "Edit Package"
+  }
+
+}     
