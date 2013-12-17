899da0c4d6762f1c3aedf655fd096051232726e7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-12-17 16:27:01 +0000

    Added refdata type property for icon display.

--- app/grails-app/domain/com/k_int/kbplus/auth/User.groovy
+++ app/grails-app/domain/com/k_int/kbplus/auth/User.groovy
@@ -2,6 +2,7 @@ package com.k_int.kbplus.auth
 
 import javax.persistence.Transient
 import com.k_int.kbplus.Org
+import com.k_int.kbplus.RefdataValue
 
 class User {
 
@@ -25,6 +26,9 @@ class User {
   SortedSet affiliations
   SortedSet roles
   Org defaultDash
+  
+  
+  RefdataValue showInfoIcon
 
   static hasMany = [ affiliations: com.k_int.kbplus.auth.UserOrg, roles: com.k_int.kbplus.auth.UserRole ]
   static mappedBy = [ affiliations: 'user', roles: 'user' ]
@@ -136,6 +140,17 @@ class User {
 
     log.debug("user granted ${perm} for ${result}")
     result
+  }
+  
+  
+  
+  transient def getUserPreferences() {
+    def userPrefs = [
+      "showInfoIcon" : (showInfoIcon?.value?.equalsIgnoreCase("Yes") ? true : false)
+    ]
+    
+    // Return the prefs.
+    userPrefs
   }
 
   def hasPerm(perm,user) {
