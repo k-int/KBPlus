a19c9bcec93fcb40c849046b3946437ea5a1aca4
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-01-23 14:22:28 +0000

    More refactoring of package upload

---
+++ app/grails-app/services/com/k_int/kbplus/PackageIngestService.groovy
@@ -0,0 +1,16 @@
+package com.k_int.kbplus
+
+import com.k_int.kbplus.*
+import org.hibernate.ScrollMode
+import java.nio.charset.Charset
+import java.util.GregorianCalendar
+
+public class PackageIngestService {
+
+  def sessionFactory
+  def propertyInstanceMap = org.codehaus.groovy.grails.plugins.DomainClassGrailsPlugin.PROPERTY_INSTANCE_MAP
+
+
+
+
+}
