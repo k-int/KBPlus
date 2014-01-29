c34964c98fd987166446943b73dc406e5d1210e0
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-01-29 11:39:59 +0000

    Changed mapping for ajax controller to deal with refdata cats containing dots. fixes #691, #690, #689, #688

--- app/grails-app/conf/UrlMappings.groovy
+++ app/grails-app/conf/UrlMappings.groovy
@@ -13,7 +13,7 @@ class UrlMappings {
     "/myInstitutions/$shortcode/$action/$id"(controller:'myInstitutions')
     "/myInstitutions/$shortcode/dashboard"(controller:'myInstitutions', action:'instdash')
 
-    "/ajax/$action?/$id?(.${format})?"(controller:'ajax')
+    "/ajax/$action?/$id?"(controller:'ajax')
 
     // "/"(controller:"home")
     "/"(view:"/publichome")
