4875ee8f771a5c2919410dc5c0a469d95ea6211b
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-02-05 15:45:25 +0000

    More props for Global Record Source

--- app/grails-app/domain/com/k_int/kbplus/GlobalRecordSource.groovy
+++ app/grails-app/domain/com/k_int/kbplus/GlobalRecordSource.groovy
@@ -7,16 +7,29 @@ class GlobalRecordSource {
 
   String identifier
   String name
+  Date haveUpTo
+  String uri
+  String principal
+  String credentials
 
   static mapping = {
                    id column:'grs_id'
               version column:'grs_version'
            identifier column:'grs_identifier'
                  name column:'grs_name'
+             haveUpTo column:'grs_have_up_to'
+                  uri column:'grs_uri'
+            principal column:'grs_principal'
+          credentials column:'grs_creds'
   }
 
   static constraints = {
-          name(nullable:true, blank:false)
+     identifier(nullable:true, blank:false)
+           name(nullable:true, blank:false)
+       haveUpTo(nullable:true, blank:false)
+            uri(nullable:true, blank:false)
+      principal(nullable:true, blank:false)
+    credentials(nullable:true, blank:false)
   }
 
 }
