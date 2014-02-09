3c77f19fa14d55754acc664ad23a7f9462715b25
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-02-09 22:42:46 +0000

    update

--- app/grails-app/services/com/k_int/kbplus/GlobalSourceSyncService.groovy
+++ app/grails-app/services/com/k_int/kbplus/GlobalSourceSyncService.groovy
@@ -37,6 +37,9 @@ class GlobalSourceSyncService {
 
   def private doOAISync(sync_job) {
     println("doOAISync");
-
+    // oai_client = new OAIClient(url:sync_job.url)
+    // oai_client.getRecordsSince(date)
+    // sync_job.maxTimestamp=oai_client.maxTimestamp
+    // save
   }
 }
