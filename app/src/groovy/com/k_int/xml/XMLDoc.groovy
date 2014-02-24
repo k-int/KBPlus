b2cfb6066f49be1b8b31f20ce6a1545f7676e3cd
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-02-24 17:00:52 +0000

    Changed so that the doc is generated straight away and added an InputStream based constructor.

--- app/src/groovy/com/k_int/xml/XMLDoc.groovy
+++ app/src/groovy/com/k_int/xml/XMLDoc.groovy
@@ -1,5 +1,6 @@
 package com.k_int.xml
 
+import javax.xml.namespace.QName
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.Transformer
 import javax.xml.transform.TransformerFactory
@@ -8,59 +9,57 @@ import javax.xml.transform.stream.StreamResult
 import javax.xml.xpath.XPath
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
-import javax.xml.namespace.QName
 
-import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.InputStreamSource
 import org.w3c.dom.Document
 
 class XMLDoc {
-  
+
   private XPath XPath
   private Document doc
-  private InputStreamSource resource
-  
+
   public XMLDoc (InputStreamSource iss) {
-    this.resource = iss
+    this (iss.inputStream)
   }
   
-  public Document getDoc() {
-    doc = doc ?: readXML()
+  public XMLDoc (InputStream is) {
+    doc = readXML(is)
   }
-  
+
   public XPath getXPath() {
     XPath = XPath ?: XPathFactory.newInstance().newXPath()
-    XPath.setNamespaceContext(new DocumentNSResolver(getDoc()))
+    XPath.setNamespaceContext(new DocumentNSResolver(doc))
     XPath
   }
-  
+
   public transform = { out ->
     Transformer transformer = TransformerFactory.newInstance().newTransformer()
 
     // Create source and result.
-    DOMSource source = new DOMSource(getDoc())
+    DOMSource source = new DOMSource(doc)
     StreamResult result = new StreamResult(out)
-    
+
     transformer.transform(source, result)
   }
-  
+
   /**
    * Read in an XML file and return the root element.
    *
    * @param file_name The filename to read relative to the /WEB-INF/resources folder.
    * @return org.w3c.dom.Element root element of the supplied XML file.
    */
-  private Document readXML () {
-    
+  private Document readXML (InputStream is) {
+
     // Create an XML document builder.
     def docFactory = DocumentBuilderFactory.newInstance()
     docFactory.setNamespaceAware(true)
     def builder = docFactory.newDocumentBuilder()
-    
+
     // Return the document element.
-    builder.parse(resource.inputStream)
+    builder.parse(is)
   }
-  
+
   public XPath (String XPath_statment, QName retType = XPathConstants.NODESET) {
-    getXPath().evaluate("${XPath_statment}", getDoc(), retType)
+    getXPath().evaluate("${XPath_statment}", doc, retType)
   }
 }
