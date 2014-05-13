badf4f4fa6f053144407735d963974d759e10692
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-05-13 09:14:50 +0100

    updated

--- batch_reports/jusp_core.sh
+++ batch_reports/jusp_core.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mysql -u k-int -pk-int KBPlus -e "\. jusp_core.sql" > jusp_core_report.tsv
 sed '1,7d' < jusp_core_report.tsv > wip
-while read rdv_value core_start_year core_end_year jusp_title_id jusp_inst_id
+while read rdv_value core_start_year core_end_year jusp_title_id jusp_inst_id tipp
 do
   # echo $core_start_year $core_end_year $jusp_title_id $jusp_inst_id
   for (( y=core_start_year; y<=core_end_year; y++ ))
   do
-    echo "$jusp_inst_id,$jusp_title_id,$y"
+    echo "$jusp_inst_id,$jusp_title_id,$y,$tipp"
   done
 done < wip
 
