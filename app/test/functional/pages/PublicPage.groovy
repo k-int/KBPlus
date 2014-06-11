6301a533f2dbd4ba87b03751b3ed71317364b759
Ioannis Latousakis <latusaki@gmail.com>
2014-06-11 14:04:33 +0100

    debug

--- app/test/functional/pages/PublicPage.groovy
+++ app/test/functional/pages/PublicPage.groovy
@@ -6,7 +6,7 @@ import geb.Page
  * Created by ioannis on 28/05/2014.
  */
 class PublicPage extends BasePage {
-    static url = "/demo/"
+    static url = "/demo"
     static at = { browser.page.title.startsWith "Knowledge Base+"  };
 
     static content = {
