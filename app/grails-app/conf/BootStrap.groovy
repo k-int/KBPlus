f89d86e434fa18a04caf5cdf43fc3bf406f8790b
Ioannis Latousakis <latusaki@gmail.com>
2014-07-14 17:02:23 +0100

    Added some more default jasper reports.

--- app/grails-app/conf/BootStrap.groovy
+++ app/grails-app/conf/BootStrap.groovy
@@ -227,7 +227,7 @@ class BootStrap {
     if(nofilesFound){
       log.debug("No reports found, adding default reports.")
         def path = "resources/jasper_reports/"
-        def reports = ["subscriptions","titles","tipp_no_url"]
+        def reports = ["floating_titles","match_coverage","no_issn_e-issn","title_no_url"]
         reports.each { fileName ->
           def filePath = path + fileName + ".jrxml"
           def inputStreamBytes = grailsApplication.parentContext.getResource("classpath:$filePath").inputStream.bytes
