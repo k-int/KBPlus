5946bd1ac33d42d4407dc2fb56d126eea51a6d33
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-01 20:36:05 +0100

    Comment out testing gokb record source

--- app/grails-app/conf/BootStrap.groovy
+++ app/grails-app/conf/BootStrap.groovy
@@ -416,20 +416,19 @@ No Host Platform URL Content
 
 
    
-   def gokb_record_source = GlobalRecordSource.findByIdentifier('gokbPackages') ?: new GlobalRecordSource(
-                                                                                         identifier:'gokbPackages',
-                                                                                         name:'GOKB',
-                                                                                         type:'OAI',
-                                                                                         haveUpTo:null,
-                                                                                         uri:'https://gokb.kuali.org/gokb/oai/packages',
-                                                                                         listPrefix:'oai_dc',
-                                                                                         fullPrefix:'gokb',
-                                                                                         principal:null,
-                                                                                         credentials:null,
-                                                                                         rectype:0)
-
-    gokb_record_source.save(flush:true, stopOnError:true);
-    log.debug("New gokb record source: ${gokb_record_source}");
+    // def gokb_record_source = GlobalRecordSource.findByIdentifier('gokbPackages') ?: new GlobalRecordSource(
+    //                                                                                       identifier:'gokbPackages',
+    //                                                                                       name:'GOKB',
+    //                                                                                       type:'OAI',
+    //                                                                                       haveUpTo:null,
+    //                                                                                       uri:'https://gokb.kuali.org/gokb/oai/packages',
+    //                                                                                       listPrefix:'oai_dc',
+    //                                                                                       fullPrefix:'gokb',
+    //                                                                                       principal:null,
+    //                                                                                       credentials:null,
+    //                                                                                       rectype:0)
+    // gokb_record_source.save(flush:true, stopOnError:true);
+    // log.debug("New gokb record source: ${gokb_record_source}");
 
   }
 }
