65fe7aad85050ff2cb2405997eac3bde55c6f9d6
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-04 11:46:33 +0100

    Added tc config

--- conf_hints/puppet/baseline.pp
+++ conf_hints/puppet/baseline.pp
@@ -3,12 +3,12 @@ include apt
 exec {
   'set-licence-selected':
   command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
-
+ 
   'set-licence-seen':
   command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
 }
 
-apt::ppa { 'ppa:webupd8team/java':
+apt::ppa { 'ppa:webupd8team/java': 
 }
 
 package { "oracle-java7-installer":
@@ -20,3 +20,15 @@ package { "oracle-java7-set-default":
   ensure => installed,
   require  => Package['oracle-java7-installer'],
 }
+
+
+package { "apache2": ensure => installed, }
+
+package { "libapache2-mod-shib2": ensure => installed, }
+
+package { "tomcat7-user": ensure => installed, }
+
+package { "libshibsp6": ensure => installed, }
+
+package { "libtcnative-1": ensure => installed, require => Package['tomcat7-user'],}
+
