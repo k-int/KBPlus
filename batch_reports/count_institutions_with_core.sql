47e5019b7d4743fd65c2ec463e58a67b81a6009e
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-25 15:42:13 +0100

    updated

--- batch_reports/count_institutions_with_core.sql
+++ batch_reports/count_institutions_with_core.sql
@@ -14,6 +14,20 @@ where ie_subscription_fk = sub.sub_id
   and rdv_id = core_status_id
 limit 10;
 
+select count(ie.ie_id), sub_org.org_name, rdv_value
+from issue_entitlement ie,
+     subscription sub,
+     org_role subscriber_org_role,
+     org sub_org,
+     refdata_value
+where ie_subscription_fk = sub.sub_id
+  and subscriber_org_role.or_sub_fk = sub.sub_id
+  and subscriber_org_role.or_roletype_fk = @subscriber_role
+  and sub_org.org_id = subscriber_org_role.or_org_fk
+  and rdv_id = IFNULL(core_status_id,229)
+group by sub_org.org_name, rdv_value;
+
+
 
 select "All subscriptions with an IE whos core status is Yes, Print, Electronic, Print+Electronic";
 
@@ -26,6 +40,7 @@ where subscriber_org_role.or_sub_fk = sub.sub_id
   and sub_org.org_id = subscriber_org_role.or_org_fk
   and exists ( select * from issue_entitlement ie where ie_subscription_fk = sub.sub_id and ie.core_status_id in (103, 105, 106, 107 ) )
 group by sub_org.org_id
+
 
 select "IE Status summary"
 
