dd4bd959fbf763367fcf5cec1ee730404e2a112b
ioannis <latusaki@gmail.com>
2014-11-19 10:47:21 +0000

    Fixes for zendesk url

--- app/test/functional/pages/PrivacyPage.groovy
+++ app/test/functional/pages/PrivacyPage.groovy
@@ -3,6 +3,7 @@ package pages
  * Created by ioannis on 28/05/2014.
  */
 class PrivacyPage extends BasePage {
+	static url = "/demo/privacy-policy"
     static at = { browser.page.title.startsWith "Privacy Policy" }
 
 }
