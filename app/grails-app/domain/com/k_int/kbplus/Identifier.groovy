a6347d3e297f1dd43fd89dae6dd71addbb583d74
latusaki@gmail.com <latusaki@gmail.com>
2015-01-28 10:43:29 +0000

    Namespace creation fix #857

--- app/grails-app/domain/com/k_int/kbplus/Identifier.groovy
+++ app/grails-app/domain/com/k_int/kbplus/Identifier.groovy
@@ -45,10 +45,7 @@ class Identifier {
       if ( namespace && qp.size() == 2) {
         ql = Identifier.findAllByNsAndValueIlike(namespace,"${qp[1]}%")
       }
-      else if(!namespace) {
-        throw new java.lang.IllegalArgumentException("No namespace '${qp[0]}' exists.")
-
-      }
+      
     }
     else {
       ql = Identifier.findAllByValueIlike("${params.q}%",params)
