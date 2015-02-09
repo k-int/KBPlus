842edde3bec3fb8e53a313811a8c31f2a241c6c6
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-02-09 15:34:12 +0000

    Added properties to invoice

--- app/grails-app/domain/com/k_int/kbplus/Invoice.groovy
+++ app/grails-app/domain/com/k_int/kbplus/Invoice.groovy
@@ -2,12 +2,25 @@ package com.k_int.kbplus
 
 class Invoice {
 
+  Date dateOfInvoice
+  Date dateOfPayment
+  Date datePassedToFinance
+  String invoiceNumber
+
   static mapping = {
-                id column:'inv_id'
-           version column:'inv_version'
+                      id column:'inv_id'
+                 version column:'inv_version'
+           dateOfInvoice column:'inv_date_of_invoice'
+           dateOfPayment column:'inv_date_of_payment'
+     datePassedToFinance column:'inv_date_passed_to_finance'
+           invoiceNumber column:'inv_number'
   }
 
   static constraints = {
+          dateOfInvoice(nullable:true, blank:false)
+          dateOfPayment(nullable:true, blank:false)
+    datePassedToFinance(nullable:true, blank:false)
+          invoiceNumber(nullable:true, blank:false)
   }
 
 }
