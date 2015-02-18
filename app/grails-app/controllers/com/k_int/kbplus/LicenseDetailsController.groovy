2ffbfe84ad7b28353c09cf77410451567176c099
latusaki@gmail.com <latusaki@gmail.com>
2015-02-18 17:30:20 +0000

    Work in progress - LicenceCustProp history visible at license, needs testing

--- app/grails-app/controllers/com/k_int/kbplus/LicenseDetailsController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/LicenseDetailsController.groovy
@@ -220,9 +220,10 @@ class LicenseDetailsController {
     result.max = params.max ?: 20;
     result.offset = params.offset ?: 0;
 
-    def qry_params = [result.license.class.name, "${result.license.id}"]
-    result.historyLines = AuditLogEvent.executeQuery("select e from AuditLogEvent as e where className=? and persistedObjectId=? order by id desc", qry_params, [max:result.max, offset:result.offset]);
-    result.historyLinesTotal = AuditLogEvent.executeQuery("select count(e.id) from AuditLogEvent as e where className=? and persistedObjectId=?",qry_params)[0];
+    def qry_params = [licClass:result.license.class.name, prop:LicenseCustomProperty.class.name,licProps:result.license.customProperties.collect{it.id.toString()}, licId:"${result.license.id}"]
+    log.debug("REQUEST PARAMS ${qry_params}")
+    result.historyLines = AuditLogEvent.executeQuery("select e from AuditLogEvent as e where (( className=:licClass and persistedObjectId=:licId) or (className = :prop and persistedObjectId in (:licProps))) order by id desc", qry_params, [max:result.max, offset:result.offset]);
+    result.historyLinesTotal = AuditLogEvent.executeQuery("select count(e.id) from AuditLogEvent as e where ( (className=:licClass and persistedObjectId=:licId) or (className = :prop and persistedObjectId in (:licProps))) ",qry_params)[0];
 
     result.todoHistoryLines = PendingChange.executeQuery("select pc from PendingChange as pc where pc.license=? order by pc.ts desc", result.license);
 
