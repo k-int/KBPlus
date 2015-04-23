580084579b9548ce0a107e6a597cc8b1ec79cafe
latusaki <latusaki@gmail.com>
2015-04-23 12:03:52 +0100

    Added another test spec, needs to be connected to the others...

---
+++ app/test/functional/AdminActionsSpec.groovy
@@ -0,0 +1,53 @@
+import geb.error.RequiredPageContentNotPresent
+import geb.spock.GebReportingSpec
+import pages.*
+import spock.lang.Stepwise
+
+@Stepwise
+class AdminActionsSpec extends GebReportingSpec {
+
+	//The following will setup everything required for this test case
+	def "Setup our database"(){
+		when:
+			to PublicPage
+			loginLink()
+			at LogInPage
+			login(Data.UserD_name, Data.UserD_passwd)
+			go "/demo/org/create"
+			$("form").name = Data.Org_name
+			$("form").impId = Data.Org_impId
+			$("form").sector = 'Higher Education'
+			$("#SubmitButton").click()
+			to ProfilePage
+			changeUserNoDash(Data.UserA_name, Data.UserA_passwd)
+			requestMembership(Data.Org_name, 'Editor')
+			changeUserNoDash(Data.UserD_name, Data.UserD_passwd)
+			manageAffiliationReq()
+			at AdminMngAffReqPage
+			approve()
+
+		then:
+			true
+			to ProfilePage
+	}
+
+	def "Go to User Merge screen and transfer affiliations from user A"(){
+		setup:
+			to ProfilePage
+		when:
+			go '/demo/admin/userMerge'
+		then:
+			browser.page.title.contains("User Merge")
+		when:
+			$('select', name:'userToKeep').value(Data.UserD_displayName); 
+			$('select', name:'userToMerge').value(Data.UserA_displayName); 
+			$('input', type:'submit').click()
+		then:
+			$('h3').text() == ("Merge "+ Data.UserA_displayName + " into "+ Data.UserD_displayName)
+		when:
+			$('input',value:'Apply').click()
+			go '/demo/myInstitutions/'+Data.Org_Url+'/dashboard'
+		then:
+			at DashboardPage
+	}
+}
