ade94f9024f4a757f8356674185dbc06ecf20128
Ioannis Latousakis <latusaki@gmail.com>
2015-06-22 09:37:43 +0100

    ONIX Single view
    ONix single view correctly forwards to comparison with ghost licence

--- app/grails-app/controllers/com/k_int/kbplus/OnixplLicenseDetailsController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/OnixplLicenseDetailsController.groovy
@@ -18,7 +18,9 @@ class OnixplLicenseDetailsController {
 //            response.sendError(401);
 //            return
 //        }
-        forward (action:'matrix', params:[Compare:"Compare", id:"compare",compareAll:true,selectedLicences:params.id],controller:"onixplLicenseCompare")
+        def ghost_licence = OnixplLicense.findByTitle(grails.util.Holders.config.onix_ghost_licence)
+        def licences = ghost_licence?[params.id,ghost_licence.id] : params.id
+        forward (action:'matrix', params:[Compare:"Compare", id:"compare",compareAll:true,selectedLicences:licences],controller:"onixplLicenseCompare")
     }
 
     @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
