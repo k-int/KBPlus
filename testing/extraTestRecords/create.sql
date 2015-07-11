9e9d55b9c42ee6278d4ff50b29295d692010681c
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-07-11 18:00:26 +0100

    Created script which will add a fictional university to the test system, and make every user on the test system an admin of that institution

---
+++ testing/extraTestRecords/create.sql
@@ -0,0 +1,18 @@
+select 'Set up some MySQL Variables to hold the IDs of our custom property definitions';
+SET @InstitutionTypeId = null,
+    @AdmRoleId = null,
+    @BorsetshireUniId = null;
+
+select 'Get IDs of known custom properties';
+
+select @InstitutionTypeId := rev_id from refdata_value where rdv_value = 'Institution';
+select @AdmRoleId := id from role where authority= 'INST_ADM';
+
+
+insert into org(org_name,sector,org_shortcode,org_type_rv_fk,date_created,last_updated) values ( 'University of Borsetshire', 'Higher Education', 'uoborsetshire', @InstitutionTypeId, now(), now() );
+
+select @BorsetshireUniId := org_id from org where org_name = 'University of Borsetshire';
+
+insert into user_org(date_actioned,date_requested,org_id,role,status,user_id,formal_role_id)
+select now(),now(),@BorsetshireUniId,'deprecated',1,id,@AdmRoleId from user
+where id not in ( select user_id from user_org where org_id = @BorsetshireUniId);
