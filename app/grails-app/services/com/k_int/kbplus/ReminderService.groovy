d6c205330c49882e0d4d983473e7b10aab71fed0
Ryan <Ryan@k-int.com>
2015-08-04 20:52:51 +0100

    Added complexity to finding and delivering reminders to users, will be far more efficient

--- app/grails-app/services/com/k_int/kbplus/ReminderService.groovy
+++ app/grails-app/services/com/k_int/kbplus/ReminderService.groovy
@@ -3,7 +3,9 @@ package com.k_int.kbplus
 import com.k_int.kbplus.auth.User
 import grails.transaction.Transactional
 import groovy.text.SimpleTemplateEngine
+import org.apache.commons.lang3.time.DateUtils
 import org.elasticsearch.common.joda.time.DateTime
+import org.elasticsearch.common.joda.time.LocalDate
 import org.springframework.context.ApplicationContext
 
 import javax.annotation.PostConstruct
@@ -33,6 +35,7 @@ class ReminderService {
         result
     }
 
+    //todo Think about limiting the search to Sub data that renewal data that is year earlier than the present data, i.e. max user can enter is 12 months
     //Perform relevant searching/joins to return all subscriptions of interest
     def getAccessibleSubsForUser(User user) {
         def result
@@ -65,26 +68,48 @@ class ReminderService {
         }
     }
 
-    def isSubInReminderRange(String unit, int amount, Date subRenewal)
+    
+    def isSubInReminderRange(ArrayList reminders, Subscription sub, LocalDate today)
     {
-        DateTime datetime = new DateTime(subRenewal)
+        result = [:]
+        result.found = 0
+        LocalDate renewalDate = new LocalDate(sub.manualRenewalDate)
+        reminders.each { r ->
+         def reminderDate = dateconvertReminderToDate(r.unit.value, r.amount, subRenewal) //e.g. sub renewal 12.11.15  rem date 3 months before = 12:8:15
+         if(isDateInReminderPeriod(reminderDate, renewalDate, today))
+         {  
+           result.found++
+           //todo add to result 
+         } 
+        }
+    }
+    
+    //Date has to be between start and end range
+    def isDateInReminderPeriod(LocalDate start, LocalDate end, LocalDate today) {
+          return !today.isBefore(start) && !today.isAfter(end);
+    }
+    
+    //Returns the calculated date i.e. Unit = Week Amount = 3 convertFrom = subRenewalDate 
+    def convertReminderToDate(String unit, int amount, LocalDate convertFrom)
+    {
         switch (unit)
         {
             case 'Day':
-                datetime.minusDays(amount)
+                return new LocalDate(convertFrom).minusDays(amount)
                 break
             case 'Week':
-                datetime.minusWeeks(amount)
+                return new LocalDate(convertFrom).minusWeeks(amount)
                 break
             case 'Month':
-                datetime.minusMonths(amount)
+                return new LocalDate(convertFrom).minusMonths(amount)
                 break
             default:
-                datetime.minusDays(amount)
+                return new LocalDate(convertFrom).minusDays(amount)
                 break
         }
     }
 
+    //todo Learn how to CACHE the Subscription data to avoid possible repeating lookups
     def runReminders() {
         if (mailService.disabled)
         {
@@ -94,12 +119,30 @@ class ReminderService {
         log.debug("Running reminder service...")
         def usersReminders = getActiveEmailRemindersByUserID()
         int userCounter    = usersReminders.size()
+        LocalDate today    = new LocalDate();
         log.debug("Presently there is ${userCounter} Users with potentially 1..* reminders")
+       
+        def subcriptionKeyToUserAndReminder = [:]
 
-        usersReminders.each { k,v ->
+        //Key = User ID Val = List of Reminders 1..* for that user!
+        //Go through each reminder a user has, lookup subscriptions accessible to user, compare each subscription to the date range 
+        usersReminders.each { k,v -> 
             log.debug("Lookup up ${v.size()}:Reminders Subscriptions for user ID:${k}  Username:${v.user.username}")
-            reminderDates = v.collect { it.}
+
+            //Dates of interest, more convenient
+           // def reminderDates = v.collect { [it.unit.value,it.amount] }
+
+            //All possible subscriptions for this user
             def availibleSubs  = getAccessibleSubsForUser(v.user)
+            
+            //List would not have anything past 12 months of present date for server load purposes!
+            //todo upon finding relevant matching, would be more efficient to add a key as Subcription ID, then add a list of arrays[sub id][[user, reminder],[]] instances as values
+            availibleSubs.each { sub ->
+                isSubInReminderRange(v,sub.manualRenewalDate)
+            }
+
+
         }
     }
+
 }
