0fd9ae5a83c21b908d916d9a850dae76f7f37e2b
Ryan <Ryan@k-int.com>
2015-08-05 17:23:04 +0100

    Reminder Feature - minor update

--- app/grails-app/services/com/k_int/kbplus/ReminderService.groovy
+++ app/grails-app/services/com/k_int/kbplus/ReminderService.groovy
@@ -42,15 +42,20 @@ class ReminderService {
 
     //todo Think about limiting the search to Sub data that renewal data that is year earlier than the present data, i.e. max user can enter is 12 months
     //Perform relevant searching/joins to return all subscriptions of interest
+    /**
+     * Grabs all Subscriptions that this user can access (e.g. authorised orgs)
+     * @param user
+     * @return List of Subscriptions
+     */
     def getAccessibleSubsForUser(User user) {
-        def result
+        def result = [] as List
 
         result
     }
 
     //Key Subscription -> [[user: u, reminder: r]]
     //Will need to add Subscription to the map sent to the template subscription: sub
-    def generateMail(Subscription sub, ArrayList userRemindersList) {
+    private def generateMail(Subscription sub, ArrayList userRemindersList) {
         log.debug("Setting up mail generation requirements for Subscription ${sub.name}...");
         def emailTemplateFile = generic? applicationContext.getResource("WEB-INF/mail-templates/subscriptionManualRenewalDateGeneric.gsp").file : applicationContext.getResource("WEB-INF/mail-templates/subscriptionManualRenewalDate.gsp").file
         def engine            = new SimpleTemplateEngine()
@@ -95,25 +100,26 @@ class ReminderService {
      * @param sub           - Current iteration of a Subscription
      * @param today         - Today's date in Joda format
      */
-    def isSubInReminderRange(ArrayList reminders, Subscription sub, LocalDate today, User u)
+    private def isSubInReminderRange(ArrayList reminders, Subscription sub, LocalDate today, User u)
     {
         def result = [:]
         result.found = false
-        LocalDate renewalDate = new LocalDate(sub.manualRenewalDate)
+        LocalDate renewalDate = new LocalDate(sub.manualRenewalDate) //std Java date
 
         reminders.each { r ->
-         def reminderDate = convertReminderToDate(r.unit.value, r.amount, subRenewal) //e.g. sub renewal 12.11.15  rem date 3 months before = 12:8:15
+         def reminderDate = convertReminderToDate(r.unit.value, r.amount, renewalDate) //e.g. sub renewal 12.11.15  rem date 3 months before = 12:8:15
          if(isDateInReminderPeriod(reminderDate, renewalDate, today))
          {  
            result.found    = true
            result.instance = (['reminder':r, 'user':u])
-           return //Break out of each, should only be one for user in this period!
+           //Break out of each, should only be one for user in this period, and pointless reminder again!
+           return
          } 
         }
     }
     
     //Date has to be between start and end range
-    def isDateInReminderPeriod(LocalDate start, LocalDate end, LocalDate today) {
+    private boolean isDateInReminderPeriod(LocalDate start, LocalDate end, LocalDate today) {
           return !today.isBefore(start) && !today.isAfter(end);
     }
 
@@ -124,7 +130,7 @@ class ReminderService {
      * @param convertFrom - subRenewalDate
      * @return LocalDate
      */
-    def convertReminderToDate(String unit, int amount, LocalDate convertFrom)
+    private LocalDate convertReminderToDate(String unit, int amount, LocalDate convertFrom)
     {
         switch (unit)
         {
@@ -145,7 +151,8 @@ class ReminderService {
 
     //todo Learn how to CACHE the Subscription data to avoid possible repeating lookups
     def runReminders() {
-        log.debug("Running reminder service...")
+        def start_time = System.currentTimeMillis();
+        log.debug("Running reminder service... Started: ${start_time}")
         if (mailService.disabled)
         {
             log.error("Unable to send reminders, mail service is disabled!")
@@ -166,12 +173,14 @@ class ReminderService {
         usersReminders.each { k,v ->
             //All possible subscriptions for this user
             def availibleSubs  = getAccessibleSubsForUser(v.user)
+
             log.debug("Lookup up ${v.size()}:Reminders for dates **  ${v.collect { [it.unit.value,it.amount] }}   ** \nSubscriptions for user ID:${k} (Username:${v.user.username}) total has ${availibleSubs.size()} ")
 
             //Iterate through available subscriptions (List would not have anything past 12 months of present date for server load purposes!)
             //Organise master list i.e. [Subscription inst][['reminder':reminder inst1, 'user': user inst1],['reminder':reminder inst2, 'user': user inst2],etc]
             availibleSubs.each { sub ->
                 def reminderAndUserInst = isSubInReminderRange(v, sub, today, v.user)
+
                 if(reminderAndUserInst.found) {
                     log.debug("Found Subscription: ${sub.name} for User: ${v.user.username} Reminder: ${v.id}")
                     addToMasterList(masterSubscriptionList, sub, reminderAndUserInst.instance)
@@ -181,6 +190,9 @@ class ReminderService {
 
         //Now a master list has been created (after going through each reminder for a user), need to process for email now!
         processMasterList(masterSubscriptionList)
+
+        def end_time = System.currentTimeMillis() - start_time
+        log.debug("Finished time taken: ${end_time}")
     }
 
     private void addToMasterList(LinkedHashMap masterSubscriptionList, Subscription sub, reminderAndUserInst) {
@@ -207,7 +219,7 @@ class ReminderService {
         log.debug("Processing master list, ready for emailing based on each Subscription")
 
         masterSubscriptionList.eachWithIndex { subscription, valueList, i ->
-            if (i % 500 == 0)
+            if (i % 5000 == 0)
                 log.debug("Proessed ${i} Subscriptions...")
 
             generateMail(subscription, valueList)
