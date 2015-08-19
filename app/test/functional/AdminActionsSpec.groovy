cc639e0aeda241a64ab0041efa3297509f62bfa8
latusaki <latusaki@gmail.com>
2015-08-19 16:28:54 +0100

    Replace button value match with id...

--- app/test/functional/AdminActionsSpec.groovy
+++ app/test/functional/AdminActionsSpec.groovy
@@ -49,8 +49,8 @@ class AdminActionsSpec extends GebReportingSpec {
     then:
       $('h3').text() == ("Merge "+ Data.UserA_displayName + " into "+ Data.UserD_displayName)
     when:
-      waitFor{$('input',value:'Apply').verifyNotEmpty()}
-      $('input',value:'Apply').click()
+      waitFor{$("#mergeUsersBtn").verifyNotEmpty()}
+      $("#mergeUsersBtn").click()
     then:
       messageBox("successful")
   }
