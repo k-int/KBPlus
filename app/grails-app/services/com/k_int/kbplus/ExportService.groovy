0647488a9b6f5926fef3e31de900831d88c7176d
latusaki <latusaki@gmail.com>
2015-09-18 16:37:53 +0100

    UPGRADE-pre to restore deleted namespace identifier fks

--- app/grails-app/services/com/k_int/kbplus/ExportService.groovy
+++ app/grails-app/services/com/k_int/kbplus/ExportService.groovy
@@ -46,7 +46,7 @@ class ExportService {
 			if(result != null && (result.institution || result.validOn || result.propertyFilter || result.keyWord)){
 				writer.write("SEARCH TERMS\n")
 				writer.write("Institution,ValidOn,ReferenceSearch,LicenceProperty,LicencePropertyValue\n")
-				writer.write("${val(result.institution.name)},${val(result.validOn)},${val(result.keyWord)},${val(result.propertyFilterType)},${val(result.propertyFilter)}\n" )
+				writer.write("${val(result.institution?.name)},${val(result.validOn)},${val(result.keyWord)},${val(result.propertyFilterType)},${val(result.propertyFilter)}\n" )
 				writer.write("\n")	
 			}
 			writer.write("KB+ Licence ID, LicenceReference,NoticePeriod,LicenceURL,LicensorRef, LicenseeRef, StartDate, EndDate, Licence Category,Licence Status")
