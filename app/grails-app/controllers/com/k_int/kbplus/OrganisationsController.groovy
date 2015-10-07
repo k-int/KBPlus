d3445069ee94aa6277a432febcf1628e08b5b48e
latusaki <latusaki@gmail.com>
2015-10-07 10:31:33 +0100

    BUGFIX org/users editable by inst_adm zendesk 1885

--- app/grails-app/controllers/com/k_int/kbplus/OrganisationsController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/OrganisationsController.groovy
@@ -136,12 +136,14 @@ class OrganisationsController {
     @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
     def users() {
       def result = [:]
-      if ( SpringSecurityUtils.ifAllGranted('ROLE_ADMIN') ) {
+      result.user = User.get(springSecurityService.principal.id)
+      def orgInstance = Org.get(params.id)
+      
+      if ( orgInstance.hasUserWithRole(result.user,'INST_ADM') ) {
         result.editable = true
       }
       def tracked_roles = ["KBPLUS_EDITOR":"KB+ Editor","ROLE_ADMIN":"KB+ Administrator"]
-      result.user = User.get(springSecurityService.principal.id)
-      def orgInstance = Org.get(params.id)
+
       if (!orgInstance) {
         flash.message = message(code: 'default.not.found.message', args: [message(code: 'org.label', default: 'Org'), params.id])
         redirect action: 'list'
